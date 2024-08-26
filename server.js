e5b87c8c912a67d88f3c150cbdc05af635500d6a
gitaman69 <166131965+gitaman69@users.noreply.github.com>
2024-08-26 19:15:23 +0530

    Your descriptive message about the changes

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const mysql = require('mysql');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Use CORS
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 
 const db = mysql.createConnection({
