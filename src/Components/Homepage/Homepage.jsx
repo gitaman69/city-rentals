f2f007db102f1d35f47ad2368a8a67f8322d4fa9
gitaman69 <166131965+gitaman69@users.noreply.github.com>
2024-09-06 21:05:43 +0530

    changes

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import { Link } from 'react-router-dom';
+import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { cities } from '../Homepage/cities';
 import '../Homepage/homepage.scss';
 
 const Homepage = ({ searchQuery }) => {
+
+  useEffect(() => {
+    AOS.init({ duration: 1000 }); // Initialize AOS with a duration of 1000ms (1 second)
+  }, []);
 
   const filteredCities = cities.filter(city =>
     city.toLowerCase().includes(searchQuery.toLowerCase())
@@ -14,7 +19,7 @@ const Homepage = ({ searchQuery }) => {
     <main className="homepage">
       <div className="city-list">
         {filteredCities.map(city => (
-          <div className="city-card" id={city.toLowerCase().replace(' ', '-')} key={city}>
+          <div className="city-card" data-aos="fade-up" id={city.toLowerCase().replace(' ', '-')} key={city}>
             <Link to={`/city/${city.toLowerCase().replace(' ', '-')}`}>
               <button className="city-button">
                 {city}
